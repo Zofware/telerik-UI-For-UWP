d489365c2580bb659564ed42373ce5e89a588ddf
Adam Zofware <adam@zofware.com>
2021-02-26 21:36:30 -0800

    Local build 1.0.1.9-z7

--- BuildTools/BuildNuGet.bat
+++ BuildTools/BuildNuGet.bat
@@ -1,2 +1,3 @@
-SET MSBUILD=%WINDIR%\microsoft.net\framework\v4.0.30319\MSBuild.exe
-%MSBUILD% BuildNuget.UWP.proj /property:Version=1.0.1.9
+rem SET MSBUILD=%WINDIR%\microsoft.net\framework\v4.0.30319\MSBuild.exe
+rem msbuild BuildNuget.UWP.proj /property:Version=1.0.1.9-z4 /property:Configuration=Debug
+msbuild BuildNuget.UWP.proj /property:Version=1.0.1.9-z7
