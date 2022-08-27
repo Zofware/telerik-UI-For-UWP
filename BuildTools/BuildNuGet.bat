34c1ee1b83a0e4fbe3c47e44be4c8f923a454cc8
Adam Zofware <adam@zofware.com>
2022-08-27 11:21:43 -0700

    1.0.2.9-z6

--- BuildTools/BuildNuGet.bat
+++ BuildTools/BuildNuGet.bat
@@ -1,3 +1,3 @@
 rem SET MSBUILD=%WINDIR%\microsoft.net\framework\v4.0.30319\MSBuild.exe
-rem msbuild BuildNuget.UWP.proj /property:Version=1.0.1.9-z4 /property:Configuration=Debug
-msbuild BuildNuget.UWP.proj /property:Version=1.0.2.9-z3
+rem Debug builds add to below: /property:Configuration=Debug
+msbuild BuildNuget.UWP.proj /property:Version=1.0.2.9-z6
